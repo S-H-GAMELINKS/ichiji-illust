b7beab96165b0252367b499594e67952f86d50d1
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-09 10:11:04 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'illusts#index'
-  resource :illusts
+  resources :illusts
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
